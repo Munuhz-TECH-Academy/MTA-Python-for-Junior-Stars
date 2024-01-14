c1b9409bb628d70d0c971e23cf8d9bcdba21bf8e
nuhuhmutebi <mutebinuuhfranklin@gmail.com>
2024-01-15 00:16:30 +0300

    Add String Indexing.py file to Module 5

---
+++ Module 5/String Indexing.py
@@ -0,0 +1,15 @@
+''' Indexing - Allow access string characters directly using a 
+numeric value 
+'''
+## Postive Index - references elements from left to right
+string1 = 'Munuhz' 
+print(string1[2])
+print(string1[5])
+
+## Negative Indexing
+string2 = "TECH"
+print(string2[-2])
+print(string2[-4])
+
+## Out of the range index
+print(string2[5])
